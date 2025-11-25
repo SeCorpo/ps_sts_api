8e2d5c80d5f37777e91332cfb3cc5a1ea460a654
semkoldewijn <sem@secorp.pro>
2025-11-25 15:13:13 +0100

    update basemodel using newest sqlalchemia syntax

--- src/core/base.py
+++ src/core/base.py
@@ -1,22 +1,43 @@
-from sqlalchemy.orm import declarative_base, DeclarativeMeta
-from sqlalchemy import Column, DateTime, Integer, ForeignKey, Boolean
-from sqlalchemy.sql import func
+from datetime import datetime
+from typing import Optional
+from sqlalchemy import DateTime, Integer, ForeignKey, Boolean, func
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
-Base: DeclarativeMeta = declarative_base()
 
 class AuditMixin:
     """ Mixin for created/updated timestamps """
-    created_at = Column("created_at", DateTime(timezone=True), server_default=func.now(), nullable=False)
-    updated_at = Column("updated_at", DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        "created_at",
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        "updated_at",
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
 
 class CreatedByMixin:
     """Mixin for created_by_user_id field """
-    created_by_user_id = Column("created_by_user_id", Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=True)
+    created_by_user_id: Mapped[Optional[int]] = mapped_column(
+        "created_by_user_id",
+        Integer,
+        ForeignKey("users.user_id", ondelete="SET NULL"),
+        nullable=True,
+    )
 
 class SoftDeleteMixin:
     """ Mixin for deleted field """
-    deleted = Column("deleted", Boolean, default=False, nullable=False)
+    deleted: Mapped[bool] = mapped_column(
+        "deleted",
+        Boolean,
+        default=False,
+        nullable=False,
+    )
 
-class BaseModel(Base, AuditMixin, CreatedByMixin, SoftDeleteMixin):
+class BaseModel(DeclarativeBase, AuditMixin, CreatedByMixin, SoftDeleteMixin):
     """ Abstract base class for all models """
     __abstract__ = True
